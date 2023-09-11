033edd1978d66931d54ff09f01c9d3841897fd69
gopiraju000555 <rajumaka911@gmail.com>
2023-09-11 23:59:26 +0530

    vairables

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        String s = "raju";
+//        RWOnly obj = new RWOnly(s);
+         // Error
+
+        RWOnly obj = new RWOnly();
+
+        String ans = obj.getter(s);
+
+
+    }
   
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,16 @@
 package com.driver;
 
+import jdk.jshell.spi.SPIResolutionException;
+
 public class RWOnly {
-    
+
+    String name;
+
+//    private RWOnly() {
+//        this.name = name;
+//    }
+    String getter(String name){
+        return name;
+    }
+
 }
